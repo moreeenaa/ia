ef299059722a6968b8249395446e0963dccb2c94
moreeenaa <moreeemoncerat@gmail.com>
2026-04-13 14:37:52 -0300

    Create 13.py

---
+++ 13.py
@@ -0,0 +1,9 @@
+import pandas as pd
+df=pd.read_csv('StudentsPerformance (1) (1).csv') 
+print(df[['gender','math score']])
+
+print(df.head(8))
+print(df.tail(4))
+print(df.dtypes)
+print(df.shape)
+print(df.count())
